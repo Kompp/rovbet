e8c18b66de16933f63fce685b54b21d73074d2e4
Fournier Alexandre <alexandre.fournier@u-psud.fr>
2019-05-18 21:37:20 +0200

    v1.2

--- launch_v2.js
+++ launch_v2.js
@@ -1,8 +1,12 @@
 const puppeteer = require('puppeteer')
+const {
+    performance,
+    PerformanceObserver
+} = require('perf_hooks');
 
 const récupérerUrls = async browser => {
     let urls = []
-    for (var pageIndex = 1; pageIndex <= 3; pageIndex++)
+    for (var pageIndex = 1; pageIndex <= 6; pageIndex++)
         urls.push("http://www.betbot.soccer/?page=" + pageIndex + "&filter=true&period=upcoming&method=betbot&likelihood=every&odds=safe")
     return urls
 }
@@ -57,6 +61,27 @@ const récupérerDonnées = async(browser, url) => {
 
         return { matchs, vainqueurs, ligues, prédictions, dates }
     })
+
+    return résultat
+}
+
+const récupérerStatut = async(browser, url) => {
+    const page = await browser.newPage()
+        //Activer le logging à l'intérieur de page.evaluate
+    page.on('console', consoleObj => console.log(consoleObj.text()))
+    await page.goto(url)
+    await page.waitFor('body')
+
+    let résultat = page.evaluate(() => {
+        if (document.getElementsByClassName("imso_mh__l-tm-sc imso_mh__scr-it imso-light-font").length == 1) { //Match terminé
+            let score1 = document.getElementsByClassName("imso_mh__l-tm-sc imso_mh__scr-it imso-light-font")[0].innerHTML
+            let score2 = document.getElementsByClassName("imso_mh__r-tm-sc imso_mh__scr-it imso-light-font")[0].innerHTML
+            return score1 + " - " + score2
+        } else {
+            return null
+        }
+    })
+
     return résultat
 }
 
@@ -64,15 +89,27 @@ const scrap = async() => {
     let browser = await puppeteer.launch({ headless: true })
     const urlList = await récupérerUrls(browser)
 
-    let matchs = [];
+    let matchs = []
     let vainqueurs = []
-    let ligues = [];
-    let prédictions = [];
+    let ligues = []
+    let prédictions = []
     let dates = []
+    let états = []
+
+    var q = new Date();
+    var m = q.getMonth();
+    var d = q.getDate();
+    var y = q.getFullYear();
+    var ajd = new Date(y, m, d);
 
     //Tester toutes les urls
-    for (var i = 0; i < urlList.length; i++) {
-        console.log("Progression : " + i + "/" + urlList.length)
+    var finirScraping = false
+    var vérifierÉtat = true //Changer 
+    var i = 0
+    var matchday
+
+    while (!finirScraping && i < urlList.length) {
+        console.log("Progression : page n°" + i + " ...")
         const retour = await récupérerDonnées(browser, urlList[i])
         for (var j = 0; j < retour.matchs.length; j++) {
             matchs.push(retour.matchs[j])
@@ -80,12 +117,28 @@ const scrap = async() => {
             ligues.push(retour.ligues[j])
             prédictions.push(retour.prédictions[j])
             dates.push(retour.dates[j])
+
+            //Scraping de l'état du match
+            if (vérifierÉtat) {
+                url = 'http://www.google.com/search?q=' + retour.matchs[j]
+                const état = await récupérerStatut(browser, url)
+                console.log(retour.matchs[j] + "/" + état)
+                vérifierÉtat = état != null
+                états.push(état)
+            }
+
+            //Cas d'arret : plus de 2 jours après ou c'est le 1er et nous le 29
+            matchday = (new Date(retour.dates[j])).getDate()
+            finirScraping = matchday >= ajd.getDate() + 2 || (matchday == 1 && ajd.getDate() <= 29)
         }
+
+        i++
         await browser.close()
         browser = await puppeteer.launch({ headless: true })
     }
+    console.log("Recherche terminée ! (" + matchday + "/" + ajd.getDate() + ")")
 
-    return { matchs, vainqueurs, ligues, prédictions, dates }
+    return { matchs, vainqueurs, ligues, prédictions, dates, états }
 }
 
 scrap()
@@ -107,8 +160,8 @@ scrap()
         app.listen(PORT);
         console.log("Serveur lancé !")
 
-        //Tache de vérification toutes les heures
-        var interval = 60 * 60 * 1000
+        //Tache de vérification toutes les 45 mn 
+        var interval = 45 * 60 * 1000
         setInterval(function() {
             console.log("-- MISE A JOUR SITE --");
             scrap().then(value => {
